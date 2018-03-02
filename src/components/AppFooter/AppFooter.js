3adaf100570c7899ba7bc1a32304247d7d3cc26d
zahar517 <5173428@mail.ru>
2018-03-02 23:06:49 +0300

    Add styled-components to AppFooter.js, delete AppFooter.css and change test

--- src/components/AppFooter/AppFooter.js
+++ src/components/AppFooter/AppFooter.js
@@ -1,17 +1,38 @@
 import React, { PureComponent } from 'react';
 import Logo from '../AppHeader/Logo-white.svg';
-import './AppFooter.css';
+import styled from 'styled-components';
+
+const Footer = styled.footer`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  background: #222;
+  color: #eee;
+  p {
+    font-size: 0.75rem;
+    margin: 1rem;  
+  }
+`;
+
+export const FooterLogo = styled.div`
+  width: 10rem;
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
 
 export class AppFooter extends PureComponent {
   render() {
     return (
-      <div className="footer">
+      <Footer>
         <p>Сделано с любовью и старанием<br />
         Автор работы: <b>Захаров Дмитрий</b>.</p>
-        <div className="footer__logo">
+        <FooterLogo>
           <img src={ Logo } alt="logo" />
-        </div>
-      </div>
+        </FooterLogo>
+      </Footer>
     );
   }
 }
